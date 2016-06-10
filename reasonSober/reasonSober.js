d689046badfbe5a8818f558cd971172dcea767d1
Josh Gachnang <josh@pcsforeducation.com>
2016-06-10 16:50:12 -0500

    Capitalize reasons sober (looks more uniform)

--- reasonSober/reasonSober.js
+++ reasonSober/reasonSober.js
@@ -40,4 +40,12 @@ angular.module('mirror')
           pickReasons();
         }
       }
+    })
+    .filter('capitalize', function() {
+      return function(input, all) {
+        var reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
+        return (!!input) ? input.replace(reg, function(txt) {
+          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }) : '';
+      }
     });
