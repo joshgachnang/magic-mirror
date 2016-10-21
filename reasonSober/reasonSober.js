ab209c8f16355edeea920a831a261b4c77e6e994
Bong Koh <bdky02@gmail.com>
2016-10-21 15:25:24 -0500

    Increased reasons sober to 5.

--- reasonSober/reasonSober.js
+++ reasonSober/reasonSober.js
@@ -31,7 +31,7 @@ angular.module('mirror')
               url: '/reasonSober'
             }).then(function successCallback(response) {
               let reasons = shuffle(response.data);
-              $scope.reasons = reasons.slice(0, 3);
+              $scope.reasons = reasons.slice(0, 5);
             });
           }
 
